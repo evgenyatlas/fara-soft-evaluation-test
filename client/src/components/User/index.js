9a1e89b61855eb84c213516775751d0a60d81157
Evgeny <gekcy@mail.ru>
2021-09-02 22:03:23 +0300

    added User component

---
+++ client/src/components/User/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+
+import './User.css';
+
+/**
+* Component for displaying a user from first two letters
+* @param {string} props.nickname 
+* @param {string} props.nickname 
+* @param {boolean} props.color 
+*/
+export function User({ className, nickname, color }) {
+    return (
+        <div className={cn("User", className)} style={{ backgroundColor: color }}>
+            {nickname.slice(0, 2)}
+        </div>
+    )
+}
+
+User.propTypes = {
+    nickname: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired
+}
