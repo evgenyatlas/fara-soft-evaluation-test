f7ac29749c6692d1d79c89f65d032959ec097484
Evgeny <gekcy@mail.ru>
2021-09-03 01:35:21 +0300

    added ChatInfo component +dependencies

---
+++ client/src/components/ChatInfo/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { declOfNum } from '../../lib/declOfNum';
+import { User } from '../User';
+import { ShareLinkBtn } from '../ShareLinkBtn';
+
+import './ChatInfo.css';
+
+export function ChatInfo({ }) {
+    const id = ''
+    const users = [1, 2];
+    return (
+        <div className="ChatInfo">
+            <div className="container">
+                <div className="ChatInfo__Info">
+                    <div className="ChatInfo__InfoName">Id: {id}</div>
+                    <span className="ChatInfo__InfoNumber">
+                        {users.length} {declOfNum(users.length, ['участник', 'участника', 'участников'])}
+                    </span>
+                </div>
+                <div className="ChatInfo__ListUsers">
+                    <User nickname="WDwdwd" color="#72d5fd"></User>
+                    <User nickname="WDwdwd" color="#72d5fd"></User>
+                </div>
+                <ShareLinkBtn className="ChatInfo__Share" />
+            </div>
+        </div>
+    )
+}
