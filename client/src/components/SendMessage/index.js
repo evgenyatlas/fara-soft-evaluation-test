e17a18cb9553bd54ba126ec077f21068a82f1ce3
Evgeny <gekcy@mail.ru>
2021-09-03 10:36:25 +0300

    added SendMessage component

---
+++ client/src/components/SendMessage/index.js
@@ -0,0 +1,24 @@
+import classNames from 'classnames'
+import cn from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Input } from '../Input'
+import { SendBtn } from '../SendBtn'
+
+import './SendMessage.css'
+
+/**
+ * SendMessage component
+ */
+export function SendMessage({ className }) {
+    return (
+        <form className={cn("SendMessage", className)}>
+            <Input className="SendMessage__Input" placeholder="Сообщение" />
+            <SendBtn className="m5-left SendMessage__Send" />
+        </form>
+    )
+}
+
+SendMessage.propTypes = {
+    className: PropTypes.string
+}
